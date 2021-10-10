3ac477a1c3dbcaa97660543f1288d9f28c287710
lodos14 <81665608+lodos14@users.noreply.github.com>
2021-10-11 03:22:19 +0900

    Add files via upload

---
+++ java/Access modifier/org/opentutorials/javatutorials/accessmodifier/CalculatorDemo.java
@@ -0,0 +1,34 @@
+package org.opentutorials.javatutorials.accessmodifier;
+
+class Calculator {
+	private int left, right;
+
+	public Calculator(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	private int _sum() {
+		return this.left + this.right;
+	}
+
+	public void sumDecoPlus() {
+		System.out.println("++++" + _sum() + "++++");
+	}
+
+	public void sumDecoMinus() {
+		System.out.println("----" + _sum() + "----");
+	}
+
+}
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		Calculator c1 = new Calculator(10, 20);
+		//c1.setOprands(10, 20);
+		c1.sumDecoPlus(); // ++++30++++	
+		c1.sumDecoMinus(); // ----30----
+	}
+
+}
